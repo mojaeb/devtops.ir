c3c239e31fd3c1e34de5a64c0082f118698161b7
mojaeb <mojaebi@gmail.com>
2021-04-13 12:11:29 +0430

    feat: add ngprogress

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,27 +1,27 @@
+import "nprogress/nprogress.css";
 import './src/styles/global.css';
 import "prismjs/themes/prism-coy.css"
-
 
 // export const onClientEntry = () => {
     // alert("fd")
     // document.getElementsByClassName("linear-progressbar-container")[0].style.visibility = 'hidden'
 // };
 
-export const onClientEntry = () => {
-    window.onload = () => {
-        console.log("loaded")
-    };
+// export const onClientEntry = () => {
+//     window.onload = () => {
+//         console.log("loaded")
+//     };
 
     // const container = document.getElementsByClassName("linear-progressbar-container");
     // container[0].display = 'block';
-};
-export const onPreRouteUpdate = () => {
-  console.log("routing")
-};
+// };
+// export const onPreRouteUpdate = () => {
+//   console.log("routing")
+// };
 
-export const onRouteUpdate = () => {
-    console.log("dsf")
-};
+// export const onRouteUpdate = () => {
+//     console.log("dsf")
+// };
 
 
 //
--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `blue`,
+        showSpinner: true,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
