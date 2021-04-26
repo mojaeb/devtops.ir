022037e3100dde2533ea6fbe186a6a5403202535
Mohamadjafar Ebrahimi <39630146+mojaeb@users.noreply.github.com>
2021-04-26 18:32:29 +0430

    fix: change theme color to default blue color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        theme_color: `#0234d4`,
         display: `standalone`,
       },
     },
